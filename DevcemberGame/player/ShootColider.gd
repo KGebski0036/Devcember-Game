26087e9cb7ff11bb68677f0f64f375a4c4ce9592
Jacek Jarosz <jacekjarosz55@gmail.com>
2021-12-04 21:36:31 +0100

    idk man

--- DevcemberGame/player/ShootColider.gd
+++ DevcemberGame/player/ShootColider.gd
@@ -1,14 +1,17 @@
 extends RayCast2D
+
+var is_visible = false
 var point 
+
+
 func _physics_process(_delta):
 	cast_to = get_local_mouse_position()
 	update()
-
 
 	if(is_colliding()):
 		point = get_collision_point() - global_position
 	else:
 		point = cast_to
 func _draw():
-	draw_line(Vector2(0,0), point , Color.royalblue)
+	if is_visible: draw_line(Vector2(0,0), point , Color.royalblue)
 
--- DevcemberGame/player/ruch.gd
+++ DevcemberGame/player/ruch.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+class_name Character
+
+
 onready var WallColider : RayCast2D = $WallColider
 
 const UP_DIRECTION := Vector2.UP
@@ -7,28 +10,37 @@ const UP_DIRECTION := Vector2.UP
 export var speed := 100.0
 
 export var max_jump_strength := 150.0
-var jump_strength = max_jump_strength
 export var maximum_jumps := 3
 export var weakening_jumps := 10.0
 export var gravity := 500.0
 
-var wallJump = 250
-var jumpWall = 175
+var jump_strength = max_jump_strength
+var is_active := false
+
+var wallJump := 250
+var jumpWall := 175
 var jumps_made := 0
 var velocity := Vector2.ZERO
 
+var horizontal_direction: float = 0
+
 func _physics_process(delta):
-	update_input(delta)
-	execute_status( update_state() )
-
-	velocity = move_and_slide(velocity, UP_DIRECTION)
-
-
-func update_input(delta):
-	var horizontal_direction = Input.get_action_strength("right") - Input.get_action_strength("left")
+	if(is_active):
+		update_input(delta)
+		execute_status( update_state() )
+		$ShootColider.is_visible = true
+	else:
+		horizontal_direction = 0
+		$ShootColider.is_visible = false
+		
+	
 	velocity.x = lerp(velocity.x, horizontal_direction * speed, 0.2)
 	velocity.y += gravity * delta
-	
+	velocity = move_and_slide(velocity, UP_DIRECTION)
+
+func update_input(delta):
+	horizontal_direction = Input.get_action_strength("right") - Input.get_action_strength("left")
+
 func update_state():
 	var states := Array() 
 
