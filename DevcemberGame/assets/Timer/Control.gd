f8b00c9d2c21351961088d69bc3d9b78bd62c3e3
KGebski0036 <kgebski68@gmail.com>
2021-12-09 21:06:29 +0100

    bug fix

--- DevcemberGame/assets/Timer/Control.gd
+++ DevcemberGame/assets/Timer/Control.gd
@@ -1,10 +1,13 @@
 extends Control
 
-onready var label = $Label
-onready var timer = $RoundTimer
+onready var label : Label = $Label
+onready var timer : Timer = $RoundTimer
 
 signal timer_out
 
+func start_counting(time):
+	timer.wait_time = time
+	timer.start()
 
 func _process(_delta):
 	label.text = "%.3f" % timer.time_left
--- DevcemberGame/player/ShootColider.gd
+++ DevcemberGame/player/ShootColider.gd
@@ -2,16 +2,17 @@ extends RayCast2D
 
 var point 
 
-
 func _physics_process(_delta):
-	cast_to = get_local_mouse_position()
-	update()
 
 	if(!get_parent().point_to_shoot):
 		if(is_colliding()):
 			point = get_collision_point() - global_position
 		else:
 			point = cast_to
-#func _draw():
-#	draw_line(Vector2(0,0), point , Color.royalblue)
+			
+	cast_to = get_local_mouse_position()
+	update()
+func _draw():
+	if(point):
+		 draw_line(Vector2(0,0), point , Color.royalblue)
 
